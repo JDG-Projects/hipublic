3b396e681ff5f440090bdf28697143789e18e0c1
DanyloOhurtsov <d.ohurtsov.fed@gmail.com>
2026-03-15 21:01:21 +0100

    minor update to payload.ts

--- src/lib/payload.ts
+++ src/lib/payload.ts
@@ -2,5 +2,8 @@ import { getPayload } from 'payload'
 import config from '@/payload.config'
 
 export async function getPayloadClient() {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is not set')
+  }
   return getPayload({ config: await config })
 }
