bdbbf62589b17b2d26161e390ce74b79a099cffd
DanyloOhurtsov <d.ohurtsov.fed@gmail.com>
2026-03-15 23:07:24 +0100

    minimal commit message

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -47,7 +47,7 @@ export default buildConfig({
   plugins: [
     vercelBlobStorage({
       enabled: Boolean(process.env.BLOB_READ_WRITE_TOKEN),
-      token: process.env.BLOB_READ_WRITE_TOKEN || '',
+      token: process.env.BLOB_READ_WRITE_TOKEN ?? '',
       collections: {
         media: true,
       },
